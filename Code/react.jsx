6086b68a0f6a42c36ee57fd8f74f93793656d93d
CodeXSavage6s <iamsavage113377@gmail.com>
2026-01-17 16:17:31 +0000

    Setup

--- Code/react.jsx
+++ Code/react.jsx
@@ -1,1 +1,8 @@
-
+function App() {
+    return (
+        <div>
+            <h1 className="text-red-500 text-md" size>Pomodoro Timer</h1>
+        </div>
+    )
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
